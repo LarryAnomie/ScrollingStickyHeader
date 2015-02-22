017bfb8ce663e353aa21be56a427693ef16ae8c6
Larry Naman <lawrence.naman@gmail.com>
2015-02-22 20:17:26 +0000

    adds shim for function.bind

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,8 @@
-/* global $, _ */
+/* global $ */
 
 (function(w, $) {
 
     'use strict';
-
-    // http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-    // http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
 
     // requestAnimationFrame polyfill by Erik MÃ¶ller. fixes from Paul Irish and Tino Zijdel
 
@@ -24,8 +21,10 @@
             window.requestAnimationFrame = function(callback, element) {
                 var currTime = new Date().getTime();
                 var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                  timeToCall);
+                var id = window.setTimeout(function() {
+                        callback(currTime + timeToCall);
+                    },
+                    timeToCall);
                 lastTime = currTime + timeToCall;
                 return id;
             };
@@ -37,6 +36,30 @@
             };
         }
     }());
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/bind#Compatibility
+    if (!Function.prototype.bind) {
+        Function.prototype.bind = function(oThis) {
+            if (typeof this !== 'function') {
+                // closest thing possible to the ECMAScript 5
+                // internal IsCallable function
+                throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+            }
+
+            var aArgs = Array.prototype.slice.call(arguments, 1),
+                fToBind = this,
+                fNOP = function() {},
+                fBound = function() {
+                    return fToBind.apply(this instanceof fNOP && oThis ? this : oThis,
+                        aArgs.concat(Array.prototype.slice.call(arguments)));
+                };
+
+            fNOP.prototype = this.prototype;
+            fBound.prototype = new fNOP();
+
+            return fBound;
+        };
+    }
 
     /**
      * A module to hide and show a header based on user scrolling
@@ -55,13 +78,17 @@
         this.$window = $(this.window);
 
         // css classes
-        this.classHidden = 'header--hidden';         // header hidden class
-        this.classVisible = 'header--visible';       // header visible class
+        this.classHidden = 'header--hidden'; // header hidden class
+        this.classVisible = 'header--visible'; // header visible class
 
         // variables
         this.ticking = false;
         this.latestScrollY = 0;
         this.previousY = 0;
+
+        this.supportPageOffset = window.pageXOffset !== undefined;
+        this.isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+
 
         // constants
         this.HEADER_HEIGHT = this.$el.height(); // gets hidden once scrolled past this point
@@ -129,7 +156,7 @@
 
     /**
      * checks whether an update is currently taking place, if not calls an update
-     * via requestAnimationFGrame
+     * via requestAnimationFrame
      */
     StickyHeader.prototype._requestTick = function() {
         if (!this.ticking) {
@@ -140,13 +167,22 @@
     };
 
     /**
+     * cross browser window scroll Y position
+     * @return {Number} Current window.scrollY position
+     */
+    StickyHeader.prototype._getY = function() {
+        var y = this.supportPageOffset ? window.pageYOffset : this.isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+        return y;
+    }
+
+    /**
      * scroll handler
      * updates perviousY record and sets latestY record to current scroll position
      * calls requestTick
      */
     StickyHeader.prototype._onScroll = function() {
         this.previousY = this.latestScrollY;
-        this.latestScrollY = window.scrollY; // current scroll position
+        this.latestScrollY = this._getY(); // current scroll position
         this._requestTick();
     };
 
